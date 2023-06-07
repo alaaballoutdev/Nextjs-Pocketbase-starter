3d2c46ca203f2094ea6a7eb65e80e065d41231dc
alaaballoutdev <alaaballout62@gmail.com>
2023-06-07 17:46:19 +0300

    add load auth from cookie

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,31 @@
 import NextAuth , {AuthOptions} from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import pocket from 'lib/PocketBaseSingleton'
+import { cookies } from 'next/headers';
 export const authOptions:AuthOptions = {
     providers: [
         CredentialsProvider({
-          // The name to display on the sign in form (e.g. "Sign in with...")
+      
           name: "Credentials",
-          // `credentials` is used to generate a form on the sign in page.
-          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-          // e.g. domain, username, password, 2FA token, etc.
-          // You can pass any HTML attribute to the <input> tag through the object.
+          
           credentials: {
             username: { label: "Username", type: "text", placeholder: "Username or email" },
-            password: { label: "Password", type: "password", placeholder:"........" }
+            password: { label: "Password", type: "password", placeholder:"•••••••••••" }
           },
           async authorize(credentials, req) {
-            // Add logic here to look up the user from the credentials supplied
+              const cookie=  cookies();
+             const cookie_object= cookie.get('pocket');
+             pocket.authStore.loadFromCookie(cookie_object?.value||'');
+             if(pocket.authStore.isValid){
+                await pocket.collection('users').authRefresh();
+                return pocket.authStore.model; 
+             }
+
             try{
-            const authData= await pocket.collection('users').authWithPassword(credentials?.username||'',credentials?.password||'');
-            return authData.record
+              
+              const authData= await pocket.collection('users').authWithPassword(credentials?.username||'',credentials?.password||'');
+              cookie.set('pocket',pocket.authStore.exportToCookie())
+              return authData.record
             }catch(error){
                 return null;
             }
